d7d82bd75d8d96b0b9a3134b7272dc621195e74e
DestinyFrog <pedrocalizto9@gmail.com>
2025-05-06 17:15:20 -0300

    fix more formating

--- web/src/windows/WindowElementView.svelte
+++ web/src/windows/WindowElementView.svelte
@@ -31,17 +31,11 @@
     // Frames
     const DELAY = 40
 
-    const WIDTH =
-        (element.atomic_radius || ATOMIC_RADIUS_DEFAULT) *
-            ATOMIC_RADIUS_WEIGHT *
-            2 +
-        BORDER * 2
-    const HEIGHT =
-        (element.atomic_radius || ATOMIC_RADIUS_DEFAULT) *
-            ATOMIC_RADIUS_WEIGHT *
-            2 +
-        BORDER * 2
+    const WIDTH = (element.atomic_radius || ATOMIC_RADIUS_DEFAULT) * ATOMIC_RADIUS_WEIGHT * 2 + BORDER * 2
+    const HEIGHT = (element.atomic_radius || ATOMIC_RADIUS_DEFAULT) * ATOMIC_RADIUS_WEIGHT * 2 + BORDER * 2
     const CENTER = { x: WIDTH / 2, y: HEIGHT / 2 }
+
+    const BLACK = '#000000'
 
     onMount(() => {
         ctx = canvas.getContext("2d")!
@@ -50,71 +44,55 @@
         canvas.width = WIDTH
         canvas.height = HEIGHT
 
+        function draw() {
+            ELETRONS_ANGLE += ELETRON_ANGULAR_SPEED
+            if (ELETRONS_ANGLE > Math.PI * 2) ELETRONS_ANGLE = 0
+
+            ctx.fillStyle = BLACK
+            ctx.fillRect(0, 0, WIDTH, HEIGHT)
+
+            let neutrons = element.neutrons
+            let protons = element.protons
+            while (neutrons > 0 || protons > 0) {
+                ctx.fillStyle = NEUTRON_COLOR
+                if (--neutrons > 0) {
+                    ctx.beginPath()
+                    const x = CENTER.x + Math.cos(Math.random() * 360) * Math.random() * CORE_RADIUS
+                    const y = CENTER.y + Math.sin(Math.random() * 360) * Math.random() * CORE_RADIUS
+                    ctx.arc(x, y, PROTON_RADIUS, 0, Math.PI * 2)
+                    ctx.fill()
+                }
+
+                if (--protons > 0) {
+                    ctx.beginPath()
+                    ctx.fillStyle = PROTON_COLOR
+                    const x = CENTER.x + Math.cos(Math.random() * 360) * Math.random() * CORE_RADIUS
+                    const y = CENTER.y + Math.sin(Math.random() * 360) * Math.random() * CORE_RADIUS
+                    ctx.arc(x, y, PROTON_RADIUS, 0, Math.PI * 2)
+                    ctx.fill()
+                }
+            }
+
+            ctx.fillStyle = ELETRON_COLOR
+            for (let i = 0; i < element.layers.length; i++) {
+                for (let j = 0; j < i; j++) {
+                    const angle = ELETRONS_ANGLE * ((j / Math.PI) * 2 * i)
+                    const distance = (((element.atomic_radius || ATOMIC_RADIUS_DEFAULT) * ATOMIC_RADIUS_WEIGHT) / i) * (1 + j) + CORE_RADIUS
+
+                    const x = CENTER.x + Math.cos(angle) * distance
+                    const y = CENTER.y + Math.sin(angle) * distance
+
+                    ctx.beginPath()
+                    ctx.arc(x, y, ELETRON_RADIUS, 0, Math.PI * 2)
+                    ctx.fill()
+                    ctx.closePath()
+                }
+            }
+
+            setTimeout(() => requestAnimationFrame(draw), DELAY)
+        }
         draw()
     })
-
-    const BLACK = "#000000"
-
-    function draw() {
-        ELETRONS_ANGLE += ELETRON_ANGULAR_SPEED
-        if (ELETRONS_ANGLE > Math.PI * 2) ELETRONS_ANGLE = 0
-
-        ctx.fillStyle = BLACK
-        ctx.fillRect(0, 0, WIDTH, HEIGHT)
-
-        let neutrons = element.neutrons
-        let protons = element.protons
-        while (neutrons > 0 || protons > 0) {
-            ctx.fillStyle = NEUTRON_COLOR
-            if (--neutrons > 0) {
-                ctx.beginPath()
-                const x =
-                    CENTER.x +
-                    Math.cos(Math.random() * 360) * Math.random() * CORE_RADIUS
-                const y =
-                    CENTER.y +
-                    Math.sin(Math.random() * 360) * Math.random() * CORE_RADIUS
-                ctx.arc(x, y, PROTON_RADIUS, 0, Math.PI * 2)
-                ctx.fill()
-            }
-
-            if (--protons > 0) {
-                ctx.beginPath()
-                ctx.fillStyle = PROTON_COLOR
-                const x =
-                    CENTER.x +
-                    Math.cos(Math.random() * 360) * Math.random() * CORE_RADIUS
-                const y =
-                    CENTER.y +
-                    Math.sin(Math.random() * 360) * Math.random() * CORE_RADIUS
-                ctx.arc(x, y, PROTON_RADIUS, 0, Math.PI * 2)
-                ctx.fill()
-            }
-        }
-
-        ctx.fillStyle = ELETRON_COLOR
-        for (let i = 0; i < element.layers.length; i++) {
-            for (let j = 0; j < i; j++) {
-                const angle = ELETRONS_ANGLE * ((j / Math.PI) * 2 * i)
-                const distance =
-                    (((element.atomic_radius || ATOMIC_RADIUS_DEFAULT) *
-                        ATOMIC_RADIUS_WEIGHT) /
-                        i) *
-                        (1 + j) +
-                    CORE_RADIUS
-
-                const x = CENTER.x + Math.cos(angle) * distance
-                const y = CENTER.y + Math.sin(angle) * distance
-
-                ctx.beginPath()
-                ctx.arc(x, y, ELETRON_RADIUS, 0, Math.PI * 2)
-                ctx.fill()
-                ctx.closePath()
-            }
-        }
-
-        setTimeout(() => requestAnimationFrame(draw), DELAY)
-    }
 </script>
 
 <Window title={capitalize(element.oficial_name)}>
